90d83ae49cf4caf7a1a0a1b92d09e3daa4500df3
Aashish1107 <aashishvennapusa1@gmail.com>
2024-08-02 15:53:01 +0530

    small tweeks on web pages

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -39,7 +39,7 @@ function Dashboard() {
     <>
       <section className='heading'>
         <h1>Welcome {user && user.name}</h1>
-        <p>Goals Dashboard</p>
+        <p>Dashboard</p>
       </section>
 
       <GoalForm />
